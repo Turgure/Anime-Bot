a2c667db8cc96f7e2a2f47fae86b7ebbfe4599ae
turgure <toranana_bye@yahoo.co.jp>
2022-09-20 02:34:47 +0900

    fix: sys.path.appendを1ファイルにまとめて呼べるようにする

--- app/src/lambda_function.py
+++ app/src/lambda_function.py
@@ -1,8 +1,5 @@
 import json
-import sys
-# tweepyをインストールしたディレクトリ相対パスを指定する
-sys.path.append('lib')
-import tweepy
+from library import tweepy
 
 # https://qiita.com/r_saiki/items/8ae6df4c808b74bd824e
 # https://qiita.com/charon/items/19ab5087f7036dafce4b
---
+++ app/src/library.py
@@ -0,0 +1,5 @@
+# ディレクトリ相対パスを指定する
+import sys
+sys.path.append('lib')
+
+import tweepy
